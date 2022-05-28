50bf9092bb1122aaba966b024ac576cff8dc84b6
meptael <meptael@gmail.com>
2022-05-28 13:46:09 +0900

    entry

---
+++ tutorials/src/login/entry.java
@@ -0,0 +1,17 @@
+package login;
+
+import java.lang.*;
+
+public class entry {
+
+	public static void main(String[] args) {
+		String ID = args[0];
+		if(ID.equals("egoing")) {
+			System.out.println("RIGHT");
+		} else {
+			System.out.println("WRONG");
+		}
+
+	}
+
+}
